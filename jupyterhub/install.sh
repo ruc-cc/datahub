3a804fff45f8980788bc44fbe930869cee42440e
Azipro <532789173@qq.com>
2021-11-05 11:12:47 +0800

    fix install, uninstall and feat ceph, ingress install

--- jupyterhub/install.sh
+++ jupyterhub/install.sh
@@ -1,2 +1,3 @@
 helm upgrade --install jupyterhub . --create-namespace -f values.yaml -n jupyterhub
-
+kubectl create -f ceph-share-pvc.yaml -n jupyterhub
+kubectl create -f ingress.yaml -n jupyterhub
--- jupyterhub/uninstall.sh
+++ jupyterhub/uninstall.sh
@@ -1,2 +1,2 @@
+helm uninstall jupyterhub -n jupyterhub
 kubectl delete namespaces jupyterhub
-
