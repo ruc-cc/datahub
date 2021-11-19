9fd3f13f18c2d5b7510635f9f5242aea7f319563
Azipro <532789173@qq.com>
2021-11-19 19:04:14 +0800

    feat https and datahub is working

--- jupyterhub/install.sh
+++ jupyterhub/install.sh
@@ -1,3 +1,7 @@
 helm upgrade --install jupyterhub . --create-namespace -f values.yaml -n jupyterhub
 kubectl create -f ceph-share-pvc.yaml -n jupyterhub
+kubectl create secret tls datahub-tls --key=/etc/cert-datahub/server.key --cert=/etc/cert-datahub/cert.pem -n jupyterhub
+kubectl create cm nginx-cm --from-file=nginx.conf -n jupyterhub
+kubectl create -f nginx-deamon.yaml -n jupyterhub
+kubectl create -f nginx-service.yaml -n jupyterhub
 kubectl create -f ingress.yaml -n jupyterhub
