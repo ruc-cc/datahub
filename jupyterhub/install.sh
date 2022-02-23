c826f4f7d1c43e775c4cd47b64d46175e9f6966a
Azipro <532789173@qq.com>
2022-02-23 14:12:09 +0800

    feat nfs

--- jupyterhub/install.sh
+++ jupyterhub/install.sh
@@ -1,5 +1,5 @@
 helm upgrade --install jupyterhub . --create-namespace -f values.yaml -n jupyterhub
-kubectl create -f ceph-share-pvc.yaml -n jupyterhub
+kubectl create -f nfs-share-pvc.yaml -n jupyterhub
 kubectl create secret tls datahub-tls --key=/etc/cert-datahub/server.key --cert=/etc/cert-datahub/cert.pem -n jupyterhub
 kubectl create cm nginx-cm --from-file=nginx.conf -n jupyterhub
 kubectl create -f nginx-deamon.yaml -n jupyterhub
