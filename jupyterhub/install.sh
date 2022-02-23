8dd7971623c690f893300a5ddbae293bbdffb072
Azipro <532789173@qq.com>
2022-02-23 19:32:30 +0800

    Tidy

--- jupyterhub/install.sh
+++ jupyterhub/install.sh
@@ -1,7 +1,7 @@
 helm upgrade --install jupyterhub . --create-namespace -f values.yaml -n jupyterhub
-kubectl create -f nfs-share-pvc.yaml -n jupyterhub
+kubectl create -f ./shareDir/nfs-share-pvc.yaml -n jupyterhub
 kubectl create secret tls datahub-tls --key=/etc/cert-datahub/server.key --cert=/etc/cert-datahub/cert.pem -n jupyterhub
-kubectl create cm nginx-cm --from-file=nginx.conf -n jupyterhub
-kubectl create -f nginx-deamon.yaml -n jupyterhub
-kubectl create -f nginx-service.yaml -n jupyterhub
-kubectl create -f ingress.yaml -n jupyterhub
+kubectl create cm nginx-cm --from-file=./nginxDeamon/nginx.conf -n jupyterhub
+kubectl create -f ./nginxDeamon/nginx-deamon.yaml -n jupyterhub
+kubectl create -f ./nginxDeamon/nginx-service.yaml -n jupyterhub
+kubectl create -f ./nginxDeamon/ingress.yaml -n jupyterhub
--- jupyterhub/upgrade_cm.sh
+++ jupyterhub/upgrade_cm.sh
@@ -1,4 +1,4 @@
 kubectl delete cm nginx-cm -n jupyterhub
-kubectl create cm nginx-cm --from-file=nginx.conf -n jupyterhub
+kubectl create cm nginx-cm --from-file=./nginxDeamon/nginx.conf -n jupyterhub
 kubectl delete ds nginx -n jupyterhub
-kubectl create -f nginx-deamon.yaml -n jupyterhub
+kubectl create -f ./nginxDeamon/nginx-deamon.yaml -n jupyterhub
